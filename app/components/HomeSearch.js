37f03a1805ede77b339c34f617316d8c83ad390a
davemflick <davemflick@yahoo.com>
2017-04-24 16:50:43 -0400

    added search bar and search type to main page

--- app/components/HomeSearch.js
+++ app/components/HomeSearch.js
@@ -5,10 +5,27 @@ export default class HomeSearch extends React.Component {
 
 	render(){
 		return (
-			<div className="homeSearch">
+			<div className="homeSearch container">
 				<video className="videoBack" poster="../app/styles/images/palmtrees.png" autoPlay="true" loop>
 					<source src="../app/styles/images/palmtrees.mp4" type="video/mp4"/>
 				</video>
+				<div className="searchBar">
+					<div className="searchOptionBtns">
+						<button className="searchOpt" type="button">Buy</button>
+						<button className="searchOpt" type="button">Rent</button>
+						<button className="searchOpt" type="button">Sell</button>
+						<button className="searchOpt" type="button">Zestimate</button>
+					</div>
+					<div className="searchField">
+						
+							<input 
+								className="searchInput" 
+								type="text" 
+								name="search" 
+								placeholder="Input an Address, City or Zip Code!"/> 
+							<button className="submitSearch" type='button'> Search </button>
+					</div>
+				</div>
 			</div>
 		)
 	}
